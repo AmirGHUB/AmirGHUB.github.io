d64d328c3e7307ebee9c56601fc5fe32e4dc2089
AmirGHUB <61433351+AmirGHUB@users.noreply.github.com>
2020-03-09 17:13:24 -0500

    new lab

--- JS/decoratemytext.js
+++ JS/decoratemytext.js
@@ -1,7 +1,19 @@
 var timer = null;
+window.onload = function () {
+    document.getElementById("b1").onclick = timerF;
+};
+window.onload = function () {
+    document.getElementById("pig").onclick = PigLatin;
+};
+window.onload = function () {
+    document.getElementById("ck").onclick = bling;
+};
+window.onload = function () {
+    document.getElementById("mak").onclick = Malkovic;
+};
 function timerF(){
 
-    if(timer===ull){
+    if(timer===null){
         timer = setInterval(Decoration,500);
     }
     else {
@@ -11,13 +23,17 @@ function timerF(){
 }
 
 function Decoration() {
-    let a = document.getElementById("ta");
 
-            a.style.fontSize = "24pt"
-            document.getElementById("ta").innerHTML = a;
+    var a = document.getElementById("ta");
+    var b = parseInt(a.style.fontSize) + 2;
+    if(!b) {
+        b = 14;
+    }
+    console.log(b);
+    a.style.fontSize = b + "pt";
+    document.getElementById("ta") .innerHTML = a;
 
-
-}
+ }
 function bling(){
     let a = document.getElementById("ck");
     let b = document.getElementById("ta");
@@ -31,4 +47,52 @@ function bling(){
         b.style.color = "black";
         b.style.textDecoration = "none";
     }
+}
+
+function PigLatin() {
+    var myText = document.getElementById('ta');
+    var ws = myText.value.split(' ').map(function(s){
+        if(isConsonantWord(s)){ return formatConsonat(s) ;}
+        else{
+            if(isVowelWord(s)){ return formatVowel(s); }
+            else{
+                return s ;
+            }
+        } }).reduce(function(t ,s ){ return t + ' ' + s},'');
+    myText.value = ws;
+}
+function Malkovic() {
+    var myText = document.getElementById('ta');
+    var words = myText.value.split(' ').map(function(s){
+        if(s.length >=5)
+        {
+            return 'Malkovich';
+        }
+        else{
+            return s;
+        }
+    }).reduce(function(t,s){
+        return t + ' ' + s;
+    },'');
+    myText.value = words;
+}
+
+function pixelsToPoints(pixels){
+    return parseInt(pixels)*72/96;
+}
+function isVowel(s){
+    var arr = ['a','e','i','o','u','A','E','I','O','U'];
+    return arr.includes(s);
+}
+function isConsonantWord(word){
+    return word.charCodeAt(0) >=65 && word.charCodeAt(0) <=122 && !isVowel(word.charAt(0));
+}
+function isVowelWord(word){
+    return isVowel(word.charAt(0));
+}
+function formatConsonat(s){
+    return s.substr(1,s.length) + s.charAt(0) + '-ay';
+}
+function formatVowel(s){
+    return s + '-ay';
 }
